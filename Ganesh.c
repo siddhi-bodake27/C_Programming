27b88cb9426d6621770f575630231bc435def69a
siddhi-bodake27 <siddhibodake000@gmail.com>
2025-10-02 22:57:44 +0530

    Add files via upload

---
+++ Ganesh.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main() {
+    printf("\n");
+    printf("        _.-'`'-._\n");
+    printf("       /`        `\\\n");
+    printf("      |           |\n");
+    printf("      |  _     _  |\n");
+    printf("      | (o)   (o) |\n");
+    printf("      |   `---'   |\n");
+    printf("      |   \\___/   |\n");
+    printf("      |___________|\n");
+    printf("        \\_______/\n");
+    printf("         `-----'\n");
+    printf("           /|\\\n");
+    printf("          / | \\\n");
+    printf("         /  |  \\\n");
+    printf("        /   |   \\\n");
+    printf("       /____|____\\\n");
+    printf("\n");
+    printf("        Shree Ganesh!\n");
+    printf("\n");
+    return 0;
+}
