e94cac8b747dfc37bdaac86bee771e6d82071f9f
siddhi-bodake27 <siddhibodake000@gmail.com>
2025-11-19 15:32:10 +0530

    Add files via upload

---
+++ ArrayDemo4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+   int Arr[4] = {10,20,30,40};
+
+   printf("Size of Arr : %lu\n",sizeof(Arr));    //20
+   
+   printf("Arr : %lu\n",Arr);  //100
+   printf("&Arr : %lu\n",&Arr);  //100
+
+   printf("Arr + 1 : %lu\n",Arr + 1);   //104
+   printf("(&Arr) + 1 : %lu\n",(&Arr) + 1);  //120
+
+   return 0;
+}
+}
